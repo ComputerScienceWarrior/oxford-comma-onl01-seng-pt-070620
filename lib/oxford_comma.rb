39d6d7f5efcaae9c8086a991266548007c1a9139
James-Ryan Stampley <computersciencewarrior@gmail.com>
2020-07-09 02:19:48 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
+#takes an array of STRINGS
 def oxford_comma(array)
-
+  if array.size == 1
+    array.join
+  elsif array.size == 2
+    array = array.join(' and ')
+  elsif array.size == 3
+    temp = "and + #{array.last}"
+    array.pop()
+    array = array.join(', ')
+  else
+    array.join
+  end
 end
